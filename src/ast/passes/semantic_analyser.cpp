d3097b89d25ceb4946e242fe26f3058e93dda5d2
Zain Asgar <zasgar@pixielabs.ai>
2022-06-03 23:04:15 -0700

    Add ability to diable bpftrace exe build
    Signed-off-by: Zain Asgar <zasgar@pixielabs.ai>

--- src/ast/passes/semantic_analyser.cpp
+++ src/ast/passes/semantic_analyser.cpp
@@ -4041,7 +4041,6 @@ bool SemanticAnalyser::update_string_size(SizedType &type,
 SizedType SemanticAnalyser::create_merged_tuple(const SizedType &left,
                                                 const SizedType &right)
 {
-<<<<<<< HEAD
   assert(left.IsTupleTy() && right.IsTupleTy() &&
          (left.GetFieldCount() == right.GetFieldCount()));
 
