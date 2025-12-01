9f88d413639cc0eedc72c3f819b088da602b4f97
Dom Del Nano <ddelnano@gmail.com>
2025-12-01 08:08:18 +0000

    Fix dfa034d that wasn't rebased properly
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>

---
+++ src/ast/passes/node_counter.cpp
@@ -0,0 +1,18 @@
+Pass CreateCounterPass()
+{
+  auto fn = [](Node &n, PassContext &ctx) {
+    NodeCounter c;
+    c.Visit(n);
+    auto node_count = c.get_count();
+    auto max = ctx.b.max_ast_nodes_;
+    LOG(V1) << "AST node count: " << node_count;
+    if (node_count >= max) {
+      LOG(ERROR) << "node count (" << node_count << ") exceeds the limit ("
+                 << max << ")";
+      return PassResult::Error("NodeCounter", "node count exceeded");
+    }
+    return PassResult::Success();
+  };
+  return Pass("NodeCounter", fn);
+}
+
--- src/ast/passes/node_counter.h
+++ src/ast/passes/node_counter.h
@@ -4,7 +4,6 @@
 #include "ast/visitors.h"
 #include "bpftrace.h"
 #include "config.h"
-#include "log.h"
 
 namespace bpftrace {
 namespace ast {
@@ -26,23 +25,7 @@ private:
   size_t count_ = 0;
 };
 
-inline Pass CreateCounterPass()
-{
-  auto fn = [](Node &n, PassContext &ctx) {
-    NodeCounter c;
-    c.Visit(n);
-    auto node_count = c.get_count();
-    auto max = ctx.b.max_ast_nodes_;
-    LOG(V1) << "AST node count: " << node_count;
-    if (node_count >= max) {
-      LOG(ERROR) << "node count (" << node_count << ") exceeds the limit ("
-                 << max << ")";
-      return PassResult::Error("NodeCounter", "node count exceeded");
-    }
-    return PassResult::Success();
-  };
-  return Pass("NodeCounter", fn);
-}
+Pass CreateCounterPass();
 
 } // namespace ast
 } // namespace bpftrace
